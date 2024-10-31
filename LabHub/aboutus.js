99482e7d81bdd22233043a72439d2abd4ed42ecc
Chandler <157402095+Cjone228@users.noreply.github.com>
2024-10-31 17:29:21 -0400

    Delete LabHub directory

--- LabHub/aboutus.js
+++
@@ -1,8 +0,0 @@
-function showMore() {
-    const moreInfo = document.getElementById('more-info');
-    if (moreInfo.classList.contains('hidden')) {
-        moreInfo.classList.remove('hidden');
-    } else {
-        moreInfo.classList.add('hidden');
-    }
-}
